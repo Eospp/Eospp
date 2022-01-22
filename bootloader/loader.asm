8c79eeb5155c19857d019ce0b7e51dde63c6d9d3
KH <1319622082@qq.com>
2022-01-22 20:44:12 +0800

    feat: loader.asm

---
+++ bootloader/loader.asm
@@ -0,0 +1,25 @@
+org 10000H
+StartEosppLoaderMsg db "Start Eospp Loader"
+
+    mov	ax,	cs
+	mov	ds,	ax
+	mov	es,	ax
+	mov	ax,	0x00
+	mov	ss,	ax
+	mov	sp,	0x7c00
+
+;=======	display on screen : Start Loader......
+
+	mov	ax,	1301h
+	mov	bx,	000fh
+	mov	dx,	0200h		;row 2
+	mov	cx,	12
+	push	ax
+	mov	ax,	ds
+	mov	es,	ax
+	pop	ax
+	mov	bp,	StartEosppLoaderMsg
+	int	10h
+
+	jmp	$
+ 
