7bca20eaf390d575c586552feb1b6bffa606e6b4
KH <1319622082@qq.com>
2022-01-27 02:05:01 +0800

    feat: kernel

---
+++ kernel/main.cpp
@@ -0,0 +1,42 @@
+
+extern "C" int main()
+{
+    int *addr = (int *)0xffff800000a00000;
+    int i;
+
+    for (i = 0; i < 1440 * 20; i++)
+    {
+        *((char *)addr + 0) = (char)0x00;
+        *((char *)addr + 1) = (char)0x00;
+        *((char *)addr + 2) = (char)0xff;
+        *((char *)addr + 3) = (char)0x00;
+        addr += 1;
+    }
+    for (i = 0; i < 1440 * 20; i++)
+    {
+        *((char *)addr + 0) = (char)0x00;
+        *((char *)addr + 1) = (char)0xff;
+        *((char *)addr + 2) = (char)0x00;
+        *((char *)addr + 3) = (char)0x00;
+        addr += 1;
+    }
+    for (i = 0; i < 1440 * 20; i++)
+    {
+        *((char *)addr + 0) = (char)0xff;
+        *((char *)addr + 1) = (char)0x00;
+        *((char *)addr + 2) = (char)0x00;
+        *((char *)addr + 3) = (char)0x00;
+        addr += 1;
+    }
+    for (i = 0; i < 1440 * 20; i++)
+    {
+        *((char *)addr + 0) = (char)0xff;
+        *((char *)addr + 1) = (char)0xff;
+        *((char *)addr + 2) = (char)0xff;
+        *((char *)addr + 3) = (char)0x00;
+        addr += 1;
+    }
+    while (1)
+    {
+    }
+}
