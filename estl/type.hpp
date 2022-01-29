a8ac4b33d2d7fdb4bec490afd0372adf5f78464f
KH <1319622082@qq.com>
2022-01-29 17:14:18 +0800

    fix: estd namespace

--- estl/type.hpp
+++ estl/type.hpp
@@ -1,4 +1,5 @@
 #pragma once
+namespace estd{
 
 using uint64_t = unsigned long long;
 using uint32_t = unsigned int;
@@ -20,3 +21,5 @@ static_assert(sizeof(int64_t) == 8,"sizeof(int64_t) != 8 bytes!");
 static_assert(sizeof(int32_t) == 4,"sizeof(int32_t) != 4 bytes!");
 static_assert(sizeof(int16_t) == 2,"sizeof(int16_t) != 2 bytes!");
 static_assert(sizeof(int8_t)  == 1,"sizeof(int8_t)  != 1 bytes!");
+
+}
