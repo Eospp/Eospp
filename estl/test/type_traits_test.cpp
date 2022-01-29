8bef48c1d915fef13972839a19e19ff2d938c29e
KH <1319622082@qq.com>
2022-01-29 17:26:35 +0800

    test: add estl test

---
+++ estl/test/type_traits_test.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+#include <type_traits.hpp>
+
+TEST(TYPE_TRAITS,IS_TEST)
+{
+    //true_type
+    EXPECT_TRUE (estd::true_type::value);
+    //false_type
+    EXPECT_FALSE(estd::false_type::value);
+    //is_pointer
+    EXPECT_FALSE(estd::is_pointer<int>::value);
+    EXPECT_TRUE (estd::is_pointer<int*>::value);
+    EXPECT_TRUE (estd::is_pointer<int**>::value);
+    //is_array
+    EXPECT_FALSE(estd::is_array<int>::value);
+    EXPECT_FALSE(estd::is_array<int*>::value);
+    EXPECT_TRUE (estd::is_array<int[5]>::value);
+    EXPECT_TRUE (estd::is_array<int[2][5]>::value);
+    //is_const
+    EXPECT_FALSE(estd::is_const<volatile int>::value);
+    EXPECT_FALSE(estd::is_const<int>::value);
+    EXPECT_TRUE (estd::is_const<const int>::value);
+    EXPECT_TRUE (estd::is_const<const volatile int>::value);
+    //is_volatile
+    EXPECT_FALSE(estd::is_volatile<int>::value);
+    EXPECT_FALSE(estd::is_volatile<const int>::value);
+    EXPECT_TRUE (estd::is_volatile<volatile int>::value);
+    EXPECT_TRUE (estd::is_volatile<const volatile int>::value);
+    //is_reference
+    EXPECT_FALSE(estd::is_reference<int>::value);
+    EXPECT_TRUE (estd::is_reference<int&>::value);
+    EXPECT_TRUE (estd::is_reference<int&&>::value);
+    EXPECT_FALSE(estd::is_rvalue_reference<int>::value);
+    EXPECT_FALSE(estd::is_rvalue_reference<int&>::value);
+    EXPECT_TRUE (estd::is_rvalue_reference<int&&>::value);
+    EXPECT_FALSE(estd::is_lvalue_reference<int>::value);
+    EXPECT_FALSE(estd::is_lvalue_reference<int&&>::value);
+    EXPECT_TRUE (estd::is_lvalue_reference<int&>::value);
+}
+
+TEST(TYPE_TRAITS,ADD_TYPE_TEST)
+{
+
+}
+
+TEST(TYPE_TRAITS,REMOVE_TYPE_TEST)
+{
+
+}
