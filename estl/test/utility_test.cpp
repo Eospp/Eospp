1b37a0042315ec20380b279d1f13e843e315c3a8
KH <1319622082@qq.com>
2022-02-09 10:22:18 +0800

    test: add compare test

--- estl/test/utility_test.cpp
+++ estl/test/utility_test.cpp
@@ -18,4 +18,14 @@ TEST(UTILITY_TEST,BASE_TEST)
 
     EXPECT_EQ((estd::make_integer_sequence<int,5>::size()),5);
     EXPECT_EQ((estd::make_index_sequence<10>::size()),10);
+
+    EXPECT_EQ((estd::plus<int>()(lhs,rhs)),3);
+    EXPECT_EQ((estd::minus<int>()(lhs,rhs)),1);
+    EXPECT_EQ((estd::multiplies<int>()(lhs,rhs)),2);
+    EXPECT_EQ((estd::divides<int>()(lhs,rhs)),2);
+    
+    EXPECT_TRUE(estd::greater<int>()(lhs,rhs));
+    EXPECT_TRUE(estd::greater_equal<int>()(lhs,rhs));
+    EXPECT_TRUE(estd::less<int>()(rhs,lhs));
+    EXPECT_TRUE(estd::less_equal<int>()(rhs,lhs));
 }
