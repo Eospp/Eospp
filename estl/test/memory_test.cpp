b9307fda9efbda3a9fc1444e5af1379ce7cefd8f
KH <1319622082@qq.com>
2022-02-12 10:54:49 +0800

    test: add shared_ptr test

--- estl/test/memory_test.cpp
+++ estl/test/memory_test.cpp
@@ -2,6 +2,10 @@
 #include <memory.hpp>
 #include <memory>
 
+struct A{};
+
+struct B : A{};
+
 TEST(UNIQUE_PTR_TEST, CONSTRUTOR_TEST) {
     // construtor
     estd::unique_ptr<int> p(new int(5));
@@ -10,10 +14,14 @@ TEST(UNIQUE_PTR_TEST, CONSTRUTOR_TEST) {
     EXPECT_FALSE(p1 == nullptr);
     EXPECT_EQ(*p, 5);
     EXPECT_EQ(*p1, 5);
+
+    estd::unique_ptr<A> p2(new B());
+    
 }
-TEST(UNIQUE_PTR_TEST, MOVE_CONSTUTOR_TEST) {
+TEST(UNIQUE_PTR_TEST, MOVE_CONSTRUTOR_TEST) {
     estd::unique_ptr<int> p = estd::make_unique<int>(5);
     estd::unique_ptr<int> p1(estd::move(p));
+    estd::unique_ptr<A> p2 = estd::make_unique<B>();
     EXPECT_TRUE(p == nullptr);
     ASSERT_TRUE(p1 != nullptr);
     EXPECT_EQ(*p1, 5);
@@ -38,6 +46,7 @@ TEST(UNIQUE_PTR_TEST, INTERFACE_TEST) {
     EXPECT_TRUE(p == nullptr);
     ASSERT_TRUE(p2 != nullptr);
     EXPECT_TRUE(*p2 == 10);
+    delete p2;
 
     p.reset(new int(77));
     ASSERT_TRUE(p != nullptr);
@@ -67,4 +76,67 @@ TEST(UNIQUE_PTR_TEST, ARRAY_TEST) {
     EXPECT_TRUE(p[4] == 4);
 
     EXPECT_TRUE((estd::is_same_v<estd::default_deleter<int[]> &, decltype(p.get_deleter())>));
+}
+
+
+TEST(SHARED_PTR_TEST, CONSTRUTOR_TEST) {
+    estd::shared_ptr<int> p = estd::make_shared<int>(5);
+    ASSERT_TRUE(p != nullptr);
+    EXPECT_TRUE(*p == 5);
+
+    estd::shared_ptr<int> p1;
+    ASSERT_TRUE(p1 == nullptr);
+
+    bool flag = false;
+    {
+        estd::shared_ptr<int> p2(new int(5), [&flag](int *p) {
+            flag = true;
+            delete p;
+        });
+
+        ASSERT_TRUE(p2 != nullptr);
+        EXPECT_TRUE(*p2 == 5);
+    }
+
+    EXPECT_TRUE(flag);
+}
+
+TEST(SHARED_PTR_TEST,COPY_CONSTRUTOR_TEST)
+{
+    estd::shared_ptr<int> p = estd::make_shared<int>(88);
+    ASSERT_TRUE(p != nullptr);
+    estd::shared_ptr<int> p1(p);
+    ASSERT_TRUE(p1 != nullptr);
+    EXPECT_TRUE(*p1 == 88);
+}
+
+
+TEST(SHARED_PTR_TEST,MOVE_CONSTRUTOR_TEST)
+{
+    estd::shared_ptr<int> p = estd::make_shared<int>(88);
+    ASSERT_TRUE(p != nullptr);
+    estd::shared_ptr<int> p1(estd::move(p));
+    ASSERT_TRUE(p == nullptr);
+    ASSERT_TRUE(p1 != nullptr);
+    EXPECT_TRUE(*p1 == 88);
+}
+
+
+TEST(SHARED_PTR_TEST,COPY_OPERATOR_TEST)
+{
+    estd::shared_ptr<int> p;
+    estd::shared_ptr<int> p1;
+    p1 = p;
+    ASSERT_TRUE(p  == nullptr);
+    ASSERT_TRUE(p1 == nullptr);
+
+    p = new int(5);
+
+    ASSERT_TRUE(p != nullptr);
+    EXPECT_TRUE(*p == 5);
+
+    p1 = p;
+    ASSERT_TRUE(p1 != nullptr);
+    EXPECT_TRUE(*p1 == 5);
+
 }
