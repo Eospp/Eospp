c5d8c652c987b314c1bfc8eac12f309bf7363604
KH <1319622082@qq.com>
2022-03-14 21:56:33 +0800

    feat: add fill fill_n

--- estl/algorithm.hpp
+++ estl/algorithm.hpp
@@ -19,4 +19,23 @@ inline size_t cstrlen(const char* str) {
     return n;
 }
 
+template<typename Iterator,typename T>
+void fill(Iterator first,Iterator end,const T &value){
+
+    while(first != end){
+        *(first++) = value;
+    }
+
+}
+
+template<typename Iterator,typename T>
+void fill_n(Iterator first,size_t count,const T &value){
+
+    while(count-- > 0){
+        *(first++) = value;
+    }
+
+}
+
+
 } // namespace estd
