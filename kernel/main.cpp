6f9750ab38f4e3b562159cb90369820ebda6fe7a
KH <1319622082@qq.com>
2022-03-16 00:24:36 +0800

    feat: add init interrupt

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,11 +1,15 @@
 #include <io/vesa.hpp>
-
+#include <core/interrupt.hpp>
+#include "util/string.hpp"
 extern "C" int main()
 {
     using namespace eospp;
     io::Vesa::Init();
-    io::printf("hello eospp!\n");
-    io::printf("start eospp!\n");
-    while(1){}
+    io::printf("start eospp\n");
+    core::Interrupt::Init();
+    int i = 0;
+    while(1){
+        io::printf("loop %d times\n",i++);
+    }
     return 0;
 }
