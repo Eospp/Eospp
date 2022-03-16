afca8cd13e3f01b6d66247443e216d97e74d0129
KH <1319622082@qq.com>
2022-03-17 00:06:43 +0800

    feat: enable_arch_ess

---
+++ kernel/src/core/arch.s
@@ -0,0 +1,23 @@
+.intel_syntax noprefix
+
+
+.global _arch_enable_sse
+
+_arch_enable_sse:
+    mov eax, 0x1
+    cpuid
+    test edx, 1<<25
+    jz .no_sse
+
+    xor rax, rax
+    mov rax, cr0
+    and ax, 0xFFFB  
+    or ax, 0x2      
+    mov cr0, rax
+    mov rax, cr4
+    or ax, 3 << 9   
+    mov cr4, rax
+
+    .no_sse:
+
+    ret
